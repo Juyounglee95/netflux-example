12e62aae13d1d95d1d24adc8170530bba304f0f9
juyounglee <dongry30@gmail.com>
2020-01-14 17:45:16 +0900

    Command Line Runner

---
+++ src/main/java/juyoung/springframework/netfluxexample/bootstrap/BootstrapCLR.java
@@ -0,0 +1,37 @@
+package juyoung.springframework.netfluxexample.bootstrap;
+
+import juyoung.springframework.netfluxexample.domain.Movie;
+import juyoung.springframework.netfluxexample.repository.MovieRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+
+import java.util.UUID;
+
+@Component
+public class BootstrapCLR implements CommandLineRunner {
+
+    private final MovieRepository movieRepository;
+
+    public BootstrapCLR (MovieRepository movieRepository){
+        this.movieRepository=  movieRepository;
+
+    }
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        //clear old data
+        movieRepository.deleteAll().block();
+
+        Flux.just("Silence of Lambdas", "AEon Flux", "Enter the Mono<void>", "The Fluxxinator",
+                "Back to the Future", "Meet the Fluxes", "Lord of Fluxes" )
+                .map(title -> new Movie(title, UUID.randomUUID().toString()))
+                .flatMap(movieRepository::save)
+                .subscribe(null, null, ()-> {
+                    movieRepository.findAll().subscribe(System.out::println);
+                });
+
+    }
+}
